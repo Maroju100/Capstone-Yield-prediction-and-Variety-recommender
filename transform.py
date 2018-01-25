89642c04c12607a0f17ba2783b82ea5d5663ae65
Anubhav Rana <anubhavrana@gmail.com>
2018-01-24 16:49:15 -0800

    completed transform.py, no testing done yet

--- transform.py
+++ transform.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
+from sklearn.cluster import KMeans
 
-def transform_orginal(df):
+def transform_orginal(df, location_df):
     '''
     Add the splitting of 'MAHARASHTRA' to four different regions.
     '''
@@ -9,9 +10,19 @@ def transform_orginal(df):
     df.loc[df['Location'] == 'BELLARY', 'Location'] =  'BALLARI'
     df['YEAR'] = df['Sown \nDate'].apply(lambda x: x.year)
     df['Sow Month'] = df['Sown \nDate'].apply(lambda x: x.month)
+    df['Sowing Week'] = df['Sowing Week'].apply(lambda x: x[-6])
 
     cluster_dict = {0:'3', 1:'4', 2:'1', 3:'', 4:'2'}
 
+    km = KMeans(n_clusters=5, random_state=0, n_init=15, max_iter=400, n_jobs=-1)
+    x = location_df[['Latitude', 'Longitude']]
+    km.fit(x)
+    clusters = km.predict(x)
+    cluster_list = [x.values[clusters == i] for i in range(5)]
+    full_cluster_list = [location_df.values[clusters == i] for i in range(5)]
+
+    vil_names = [location_df[clusters == i]['Village'].values for i in range(5)]
+    
     for i in range(5):
         #df2.loc[df2['Village'].values in vil_names[i], 'Location'] = 'MAHARASHTRA'+ cluster_dict[i]
         df['Location'] = df['Village'].apply(lambda x:
@@ -52,6 +63,48 @@ def merge_transform(df, rainfall_df, altitude_df):
     final_df = pd.merge(rain_org_df, altitude_df, on=['Location'])
 
     return final_df
+
+def featurize(df, X_cols, y_col, dummy_cols):
+    '''
+    '''
+    X_initial = df[X_cols]
+    y = df[y_col]
+
+    X = pd.get_dummies(X_initial, columns=dummy_cols)
+
+    X['Sown \nDate'] = X['Sown \nDate'].apply(lambda x:
+                                                            int(
+                                                            time.mktime(
+                                                            x.timetuple())
+                                                            / 86400
+                                                            ) -
+                                                            int(
+                                                            time.mktime(
+                                                            datetime(
+                                                            x.timetuple()[0],
+                                                            1, 1)
+                                                            )
+                                                            / 86400)
+                                                            )
+
+    X['Harvest Date'] = X['Harvest Date'].apply(lambda x:
+                                                              int(
+                                                              time.mktime(
+                                                              x.timetuple())
+                                                              / 86400
+                                                              ) -
+                                                              int(
+                                                              time.mktime(
+                                                              datetime(
+                                                              x.timetuple()[0],
+                                                               1, 1))
+                                                               / 86400)
+                                                              )
+
+    return X, y
+
+
+
 
 #AFTER merge_transform, THE DATAFRAME HAS BEEN MERGED WITH RAINFALL AND ALTITUDE
 #THE RAINFALL HAS BEEN CALCULATED AND MERGED ON LOCATION AND YEAR, THE ALT IS
