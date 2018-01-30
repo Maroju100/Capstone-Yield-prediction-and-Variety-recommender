fb67f28e4af3ac86d5906295324f58307f8220aa
Anubhav Rana <anubhavrana@gmail.com>
2018-01-29 16:19:41 -0800

    changes to temperature.py

--- temperature.py
+++ temperature.py
@@ -2,9 +2,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import csv
-
-
-
+from io import BytesIO, StringIO
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
@@ -17,6 +15,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as expected
 from selenium.webdriver.support.wait import WebDriverWait
+import multiprocessing as mp
 
 def get_temp(village, location, browser, state_dict):
 
@@ -33,34 +32,110 @@ def get_temp(village, location, browser, state_dict):
 
     browser.find_element_by_css_selector("input.button.radius").click()
     time.sleep(2)
-    browser.find_element_by_css_selector("a.contentTabActive.brTop5").click()
+
+    browser.get(browser.current_url.replace('Daily', 'Monthly'))
+    time.sleep(2)
+    #browser.find_element_by_css_selector("a.contentTabActive.brTop5").click()
+    soup = BeautifulSoup(browser.page_source, 'lxml')
 
     data_max = []
     data_min = []
     year = int(soup.find('h2', {'class': 'history-date'}).get_text()[-4:])
 
-    data_max.append(['YEAR','Village','1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'])
-    data_min.append(['YEAR','Village','1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'])
-
-    while '2016' not in browser.current_url.split('?')[0]:
+    while '2018' not in browser.current_url.split('?')[0]:
         row_max = []
         row_min = []
 
-        soup = BeautifulSoup(browser.page_source, 'lxml')
-        
-        row_max.append(int(soup.find('h2', {'class': 'history-date'}).get_text()[-4:]))
-        row_max.append('Ballari')
 
-        row_min.append(int(soup.find('h2', {'class': 'history-date'}).get_text()[-4:]))
-        row_min.append('Ballari')
+
+        row_max.append(int(soup.find('h2',
+                                    {'class': 'history-date'}).get_text()[-4:]))
+        row_max.append(village)
+
+        row_min.append(int(soup.find('h2',
+                                    {'class': 'history-date'}).get_text()[-4:]))
+        row_min.append(village)
 
         for i in range(12):
+
+            soup = BeautifulSoup(browser.page_source, 'lxml')
             row_max.append(float(temp[0].get_text()))
-            row_min.append(float(temp[-1].get_text()))
+            row_min.append(float(temp[5].get_text()))
             browser.find_element_by_class_name("next-link").click()
             time.sleep(2)
-        #row = ['Ballari', float(temp[0].get_text()), float(temp[-1].get_text())]
+
         data_max.append(row_max)
         data_min.append(row_min)
 
-    return data
+    return data_max, data_min
+
+def write_temp(village_dict, browser, state_dict, s3, kind='max'):
+
+    with StringIO() as f:
+        wr = csv.writer(f)
+        data_max.append(['YEAR','Village','1', '2', '3', '4', '5',
+                         '6', '7', '8', '9', '10', '11', '12'])
+
+        wr.writerow(['YEAR','Village','1', '2', '3', '4', '5',
+                         '6', '7', '8', '9', '10', '11', '12'])
+
+        for village, location in village_dict.items():
+            data_max, data_min = get_temp(village, location,
+                                          browser, state_dict)
+            if kind == 'max':
+                wr.writerows(data_max)
+
+            elif kind == 'min':
+                wr.writerows(data_min)
+
+
+            s3.put_object(Bucket='capstone-web-scrape',
+                          Key='village_temp_data_{}.csv'.format(kind),
+                          Body=f.getvalue())
+    return None
+
+if __name__ == '__main__':
+    #df = pd.read_csv('village_names.csv')
+    #places = df[df['Location'] == 'MAHARASHTRA']['Village'].unique()
+
+
+    loc_dict = {'MAHARASHTRA': 'JAKAPUR', 'MAHARASHTRA1': 'GOLEGAON',
+                'MAHARASHTRA2': 'KAKANDI', 'MAHARASHTRA3': 'GANPUR',
+                'MAHARASHTRA4': 'DAHEGAON' }
+
+    start = time.time()
+
+    df = pd.read_csv('village_names.csv')
+    #places = df['Location'].unique()
+    df.drop(columns='Unnamed: 0', inplace=True)
+    location_groups = df.groupby(by='Location')
+    village_dict = {}
+
+    for loc, group in location_groups:
+        for vill in set(group['Village'].values):
+            village_dict[vill] = loc
+
+    states = ['KARNATAKA', 'ANDHRA PRADESH', 'ANDHRA PRADESH', 'MAHARASHTRA',
+              'ANDHRA PRADESH', 'ANDHRA PRADESH','TAMILNADU', 'TELANGANA',
+              'ANDHRA PRADESH', 'ANDHRA PRADESH', 'ANDHRA PRADESH',
+              'TELANGANA', 'TELANGANA', 'ANDHRA PRADESH']
+
+    state_dict = dict(zip(df['Location'].unique(), states))
+
+    options = Options()
+    options.add_argument('-headless')
+    browser1 = Firefox(executable_path='geckodriver', firefox_options=options)
+    browser2 = Firefox(executable_path='geckodriver', firefox_options=options)
+    s3 = boto3.client('s3')
+
+    processes = [mp.Process(target=write_temp,
+                            args=(village_dict, browser1,
+                                  state_dict, s3, kind='max')),
+                 mp.Process(target=write_temp,
+                            args=(village_dict, browser2,
+                                  state_dict, s3, kind='min'))]
+    for p in processes:
+        p.start()
+
+
+    print (time.time() - start)
